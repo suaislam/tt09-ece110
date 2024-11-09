3ef2b1e72a8cf0da5b829e63751b8338c37b9b4b
suaislam <130002901+suaislam@users.noreply.github.com>
2024-11-09 05:48:03 +0000

    please

--- src/tt_um_systolicLif.v
+++ src/tt_um_systolicLif.v
@@ -18,51 +18,124 @@ module tt_um_systolicLif (
 
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out [6:0] = 0;
+  assign uio_out [7:0] = 0;
   assign uio_oe  = 8'b10000000;
+  assign uo_out [3:0] = 0;
 
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, 1'b0};
   //reg [6:0] mac_block_One_State;
   reg [0:0] bypass_Mac;
-  wire [6:0] accumalate_Array [7:0];
-  reg [6:0] lif_IN [7:0];
+  wire [6:0] mac_Out_one, mac_Out_two, mac_Out_three, mac_Out_four;
+  wire [6:0] lif_byp_One, lif_byp_Two, lif_byp_Three, lif_byp_Four;
+  reg [0:0] lif_IN, Lif_IN_Two, Lif_IN_Three, Lif_IN_Four;
   reg [7:0]   state;
   reg [7:0] threshold;
   wire [7:0]  next_state;
   wire [0:0] spike;
 
-  assign accumalate_Array[0] = 7'b0;
 
-genvar i;
-generate 
-for(i = 1; i < 6'd7; i = i + 1) begin 
-    weight_stationary_Mac generated(
+  weight_stationary_Mac first(
         .clk(clk),
         .reset_n(rst_n),
         .x_data(ui_in[6:0]),
-        .in_weight(uio_in[6:0]),
-        .accumulate(accumalate_Array[i]),
-        .out_data(accumalate_Array[i+1]),
-        .selct_IN(bypass_Mac)
+        .in_weight(7'd16),
+        .accumulate(0),
+        .out_data(mac_Out_one),
+        .selct_IN(lif_IN)
     );
-    always @(*) begin
-        lif_IN[i] = bypass_Mac ? accumalate_Array[i] : 7'b0;
-    end
-  
-    lif one(.current({lif_IN[i],1'b0}),
+    weight_stationary_Mac second(
         .clk(clk),
         .reset_n(rst_n),
-        .state(uo_out),
-        .spike(uio_out[7])
+        .x_data(ui_in[6:0]),
+        .in_weight(7'd16),
+        .accumulate(mac_Out_one),
+        .out_data(mac_Out_two),
+        .selct_IN(Lif_IN_Two)
+    );
+    weight_stationary_Mac third(
+        .clk(clk),
+        .reset_n(rst_n),
+        .x_data(ui_in[6:0]),
+        .in_weight(7'd16),
+        .accumulate(mac_Out_two),
+        .out_data(mac_Out_three),
+        .selct_IN(Lif_IN_Three)
+    );
+    weight_stationary_Mac fourth(
+        .clk(clk),
+        .reset_n(rst_n),
+        .x_data(ui_in[6:0]),
+        .in_weight(7'd16),
+        .accumulate(mac_Out_three),
+        .out_data(mac_Out_four),
+        .selct_IN(Lif_IN_Four)
+    );
+
+    assign lif_byp_One = lif_IN ? mac_Out_one : 7'b0;
+    assign lif_byp_Two  = Lif_IN_Two ? mac_Out_two : 7'b0;
+    assign lif_byp_Three = Lif_IN_Three ? mac_Out_three : 7'b0;
+    assign lif_byp_Four  = Lif_IN_Four ? mac_Out_four : 7'b0;
+
+     lif one(.current({lif_byp_One,1'b0}),
+        .clk(clk),
+        .reset_n(rst_n),
+        .state(),
+        .spike(uo_out[7])
    
     );
+
+  lif two(.current({lif_byp_Two,1'b0}),
+        .clk(clk),
+        .reset_n(rst_n),
+        .state(),
+        .spike(uo_out[6])
+   
+    );
+     lif three(.current({lif_byp_Three,1'b0}),
+        .clk(clk),
+        .reset_n(rst_n),
+        .state(),
+        .spike(uo_out[5])
+   
+    );
+     lif four(.current({lif_byp_Four,1'b0}),
+        .clk(clk),
+        .reset_n(rst_n),
+        .state(),
+        .spike(uo_out[4])
+   
+    );
+
+// genvar i;
+// generate 
+// for(i = 1; i < 6'd7; i = i + 1) begin 
+//     weight_stationary_Mac generated(
+//         .clk(clk),
+//         .reset_n(rst_n),
+//         .x_data(ui_in[6:0]),
+//         .in_weight(uio_in[6:0]),
+//         .accumulate(accumalate_Array[i]),
+//         .out_data(accumalate_Array[i+1]),
+//         .selct_IN(bypass_Mac)
+//     );
+//     always @(*) begin
+//         lif_IN[i] = bypass_Mac ? accumalate_Array[i] : 7'b0;
+//     end
+  
+//     lif one(.current({lif_IN[i],1'b0}),
+//         .clk(clk),
+//         .reset_n(rst_n),
+//         .state(uo_out),
+//         .spike(uio_out[7])
+   
+//     );
     
   
-end
+// end
 
-endgenerate 
+// endgenerate 
 
 // genvar j;
 // generate
